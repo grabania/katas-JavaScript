7c70d9e97fb638cd68bedb4130032d31e3c38ac1
grabania <grabania.work@gmail.com>
2020-04-06 10:24:40 +0100

    Create string repeat

--- katas_8kyu.js
+++ katas_8kyu.js
@@ -49,3 +49,16 @@ console.log(removeChar('eloquent'));
 console.log(removeChar('country'));
 console.log(removeChar('person'));
 console.log(removeChar('place'));
+
+// String repeat
+// Write a function called repeatString which repeats the given String src exactly count times.
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+function repeatStr(n, s) {
+	return s.repeat(n);
+}
+
+console.log(repeatStr(3, '*'));
+console.log(repeatStr(5, '#'));
+console.log(repeatStr(2, 'ha '));

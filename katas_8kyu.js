913c36b6e0a7ab206da0e9827d2c6b5c5485e3c8
grabania <grabania.work@gmail.com>
2020-04-09 22:29:16 +0100

    Create countSheep

--- katas_8kyu.js
+++ katas_8kyu.js
@@ -62,3 +62,47 @@ function repeatStr(n, s) {
 console.log(repeatStr(3, '*'));
 console.log(repeatStr(5, '#'));
 console.log(repeatStr(2, 'ha '));
+
+// Consider an array / list of sheep where some sheep may be missing from their place.
+// We need a function that counts the number of sheep present in the array(true means present).
+// For example,
+// 	[true, true, true, false,
+// 		true, true, true, true,
+// 		true, false, true, false,
+// 		true, false, false, true,
+// 		true, true, true, true,
+// 		false, false, true, true]
+// The correct answer would be 17.
+// Hint: Don't forget to check for bad values like null/undefined
+
+function countSheeps(arrayOfSheeps) {
+	return arrayOfSheeps.filter(Boolean).length;
+}
+
+var array1 = [
+	true,
+	true,
+	true,
+	false,
+	true,
+	true,
+	true,
+	true,
+	true,
+	false,
+	true,
+	false,
+	true,
+	false,
+	false,
+	true,
+	true,
+	true,
+	true,
+	true,
+	false,
+	false,
+	true,
+	true
+];
+console.log(countSheeps(array1));

8285dc191b096c737bc14ecfa21de8b929553c15
grabania <grabania.work@gmail.com>
2020-04-11 22:31:54 +0100

    Create squareSum

--- katas_8kyu.js
+++ katas_8kyu.js
@@ -126,3 +126,17 @@ var summation = function(num) {
 
 console.log(summation(1));
 console.log(summation(8));
+
+// Square(n) Sum
+// Complete the square sum function so that it squares each number
+// passed into it and then sums the results together.
+// For example, for [1, 2, 2] it should return 9 because 1 ^ 2 + 2 ^ 2 + 2 ^ 2 = 9.
+
+function squareSum(numbers) {
+	return numbers.reduce(function(sum, n) {
+		return n ** 2 + sum;
+	}, 0);
+}
+
+console.log(squareSum([ 1, 2 ]));
+console.log(squareSum([ 0, 3, 4, 5 ]));

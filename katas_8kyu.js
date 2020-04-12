55623d4a821eb49d37f66d385e103e679b308673
grabania <grabania.work@gmail.com>
2020-04-12 18:19:22 +0100

    Create Century From Year

--- katas_8kyu.js
+++ katas_8kyu.js
@@ -169,3 +169,27 @@ console.log(basicOp('+', 4, 7));
 console.log(basicOp('-', 15, 18));
 console.log(basicOp('*', 5, 5));
 console.log(basicOp('/', 49, 7));
+
+// Century From Year
+// The first century spans from the year 1 up to and including the year 100,
+// The second - from the year 101 up to and including the year 200, etc.
+// Task :
+// Given a year, return the century it is in.
+// Input, Output
+// Examples::
+// centuryFromYear(1705)  returns(18)
+// centuryFromYear(1900)  returns(19)
+// centuryFromYear(1601)  returns(17)
+// centuryFromYear(2000)  returns(20)
+
+function century(year) {
+	var x = Math.floor(year / 100);
+	if (year % 100 === 0) return x;
+	else return x + 1;
+}
+
+console.log(century(1705), 'Testing for year 1705');
+console.log(century(1900), 'Testing for year 1900');
+console.log(century(1601), 'Testing for year 1601');
+console.log(century(2000), 'Testing for year 2000');
+console.log(century(89), 'Testing for year 89');

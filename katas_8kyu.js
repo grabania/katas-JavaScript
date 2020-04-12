4aad09eb49d43661ba46cc896585c58df2f8f0e5
grabania <grabania.work@gmail.com>
2020-04-12 18:37:33 +0100

    Update Century From Year. The shortest version.

--- katas_8kyu.js
+++ katas_8kyu.js
@@ -205,3 +205,14 @@ console.log(century(1900), 'Testing for year 1900');
 console.log(century(1601), 'Testing for year 1601');
 console.log(century(2000), 'Testing for year 2000');
 console.log(century(89), 'Testing for year 89');
+
+// Century From Year. The Shortest version.
+function century(year) {
+	return Math.ceil(year / 100); //using ceiling method to round up to nearest century (100)
+}
+
+console.log(century(1705), 'Testing for year 1705');
+console.log(century(1900), 'Testing for year 1900');
+console.log(century(1601), 'Testing for year 1601');
+console.log(century(2000), 'Testing for year 2000');
+console.log(century(89), 'Testing for year 89');

7618dd1622f6f511f8d194b5c61562a2ca71729c
grabania <grabania.work@gmail.com>
2020-04-12 18:27:06 +0100

    Update Centuary From Year

--- katas_8kyu.js
+++ katas_8kyu.js
@@ -193,3 +193,15 @@ console.log(century(1900), 'Testing for year 1900');
 console.log(century(1601), 'Testing for year 1601');
 console.log(century(2000), 'Testing for year 2000');
 console.log(century(89), 'Testing for year 89');
+
+// Century From Year. Shorter version.
+
+function century(year) {
+	return Math.floor((year - 1) / 100) + 1;
+}
+
+console.log(century(1705), 'Testing for year 1705');
+console.log(century(1900), 'Testing for year 1900');
+console.log(century(1601), 'Testing for year 1601');
+console.log(century(2000), 'Testing for year 2000');
+console.log(century(89), 'Testing for year 89');

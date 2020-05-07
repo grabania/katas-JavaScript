c1b510d35761ad7dd393c2c034d6ba3b707c7b02
grabania <grabania.work@gmail.com>
2020-05-07 22:42:53 +0100

    Add convert number to reversed array of digits

--- katas_8kyu.js
+++ katas_8kyu.js
@@ -359,8 +359,35 @@ function greet(name) {
 // 	return "Hello, " + name + "!";
 // }
 
-
 console.log(greet('Jim'), 'Hello, Jim!');
 console.log(greet('Jane'), 'Hello, Jane!');
 console.log(greet('Simon'), 'Hello, Simon!');
 console.log(greet('Johnny'), 'Hello, my love!');
+
+
+// Convert number to reversed array of digits
+// Given a random number:
+// C#: long;
+// C++: unsigned long;
+// You have to return the digits of this number within an array in reverse order.
+// Example:
+// 348597 => [7, 9, 5, 8, 4, 3]
+
+function digitize(n) {
+	return String(n).split('').map(Number).reverse();
+}
+//  or:
+
+// function digitize(n) {
+// 	return n.toString()
+// 		.split("")
+// 		.reverse()
+// 		.map((x) => Number(x));
+// }
+
+// or:
+// function digitize(n) {
+// 	return (n + '').split('').map(Number).reverse();
+// }
+
+console.log(digitize(35231), [ 1, 3, 2, 5, 3 ]);

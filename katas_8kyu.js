3b62373c0c2e98a864374a9362f95ecf6ad9a22e
grabania <grabania.work@gmail.com>
2020-05-07 22:20:49 +0100

    Update Jenny's secret message

--- katas_8kyu.js
+++ katas_8kyu.js
@@ -352,6 +352,14 @@ function greet(name) {
 	return 'Hello, ' + (name == 'Johnny' ? 'my love' : name) + '!';
 }
 
+// or:
+// function greet(name) {
+// 	if (name === "Johnny")
+// 		return "Hello, my love!";
+// 	return "Hello, " + name + "!";
+// }
+
+
 console.log(greet('Jim'), 'Hello, Jim!');
 console.log(greet('Jane'), 'Hello, Jane!');
 console.log(greet('Simon'), 'Hello, Simon!');

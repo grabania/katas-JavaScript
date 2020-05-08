9f139e25b5b0b9f31d1a2c1c1ff1e0d9e5ce83a1
grabania <grabania.work@gmail.com>
2020-05-08 17:57:14 +0100

    Add Beginner - Lost Without a Map

--- katas_8kyu.js
+++ katas_8kyu.js
@@ -364,7 +364,6 @@ console.log(greet('Jane'), 'Hello, Jane!');
 console.log(greet('Simon'), 'Hello, Simon!');
 console.log(greet('Johnny'), 'Hello, my love!');
 
-
 // Convert number to reversed array of digits
 // Given a random number:
 // C#: long;
@@ -391,3 +390,17 @@ function digitize(n) {
 // }
 
 console.log(digitize(35231), [ 1, 3, 2, 5, 3 ]);
+
+// Beginner - Lost Without a Map
+// Given an array of integers, return a new array with each value doubled.
+// For example:
+// [1, 2, 3]-- > [2, 4, 6]
+// For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know.
+
+function maps(x) {
+	return x.map((n) => n * 2);
+}
+
+console.log(maps([ 1, 2, 3 ]), [ 2, 4, 6 ]);
+console.log(maps([ 4, 1, 1, 1, 4 ]), [ 8, 2, 2, 2, 8 ]);
+console.log(maps([ 2, 2, 2, 2, 2, 2 ]), [ 4, 4, 4, 4, 4, 4 ]);

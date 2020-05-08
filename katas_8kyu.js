4c4c6e1414f78d4c23e0e35010528a8f0d35cf75
grabania <grabania.work@gmail.com>
2020-05-08 23:58:42 +0100

    Add Do I get a bonus?

--- katas_8kyu.js
+++ katas_8kyu.js
@@ -446,3 +446,35 @@ console.log(reverseWords("yoda doesn't speak like this"), "this like speak doesn
 console.log(reverseWords('foobar'), 'foobar');
 console.log(reverseWords('kata editor'), 'editor kata');
 console.log(reverseWords('row row row your boat'), 'boat your row row row');
+
+// Do I get a bonus?
+// It's bonus time in the big city! The fatcats are rubbing their paws in anticipation...
+// but who is going to make the most money?
+// Build a function that takes in two arguments(salary, bonus).
+// Salary will be an integer, and bonus a boolean.
+// If bonus is true, the salary should be multiplied by 10.
+// If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+// Return the total figure the individual will receive as a string prefixed
+// with "£"(= "\u00A3", JS, Go, and Java), "$"(C#, C++, Ruby, Clojure, Elixir, PHP and Python, Haskell, Lua)
+// or "¥"(Rust).
+
+function bonusTime(salary, bonus) {
+	return bonus === true ? '\u00A3' + salary * 10 : '\u00A3' + salary;
+}
+
+// or:
+// function bonusTime(salary, bonus) {
+// 	if (bonus === true) {
+// 		return '\u00A3' + salary * 10;
+// 	} else {
+// 		return '\u00A3' + salary;
+// 	}
+// }
+
+console.log(bonusTime(10000, true), '£100000');
+console.log(bonusTime(25000, true), '£250000');
+console.log(bonusTime(10000, false), '£10000');
+console.log(bonusTime(60000, false), '£60000');
+console.log(bonusTime(2, true), '£20');
+console.log(bonusTime(78, false), '£78');
+console.log(bonusTime(67890, true), '£678900');

9d141c13fff42bd4093003384999ea7557af51d9
grabania <grabania.work@gmail.com>
2020-05-30 23:58:43 +0100

    Add sum without highest and lowest number

--- katas_8kyu.js
+++ katas_8kyu.js
@@ -483,3 +483,19 @@ console.log(bonusTime(60000, false), '£60000');
 console.log(bonusTime(2, true), '£20');
 console.log(bonusTime(78, false), '£78');
 console.log(bonusTime(67890, true), '£678900');
+
+//Sum without highest and lowest number
+//Sum all the numbers of the array (in F# and Haskell you get a list) except the highest and the lowest element (the value, not the index!).
+// (The highest / lowest element is respectively only one element at each edge, even if there are more than one with the same value!)
+// Example:
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+// If array is empty, null or None, or if only 1 Element exists, return 0.
+
+function sumArray(array) {
+	return !array || array.length == 0
+		? 0
+		: array.sort((a, b) => a - b).slice(1, array.length - 1).reduce((res, n) => res + n, 0);
+}
+
+console.log(sumArray([ 6, 2, 1, 8, 10 ]), 16);

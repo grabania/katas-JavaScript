2d339be9963fcde4abe5950ff70fd5d82cda4b50
grabania <grabania.work@gmail.com>
2020-06-02 23:36:26 +0100

    Add Disemvowel Trolls

--- katas_8kyu.js
+++ katas_8kyu.js
@@ -536,3 +536,25 @@ console.log(getMiddle('A'), 'A');
 // function getMiddle(s) {
 // 	return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
 // }
+
+//Disemvowel Trolls
+// Trolls are attacking your comment section!
+// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+// Note: for this kata y isn't considered a vowel.
+
+disemvowel('bahumbug!');
+
+function disemvowel(str) {
+	let vowels = 'aeiouAEIOU';
+	disem = '';
+	for (let i = 0; i < str.length; i++) {
+		if (vowels.indexOf(str[i]) === -1) {
+			disem += str[i];
+		}
+	}
+	return disem;
+}
+
+console.log(disemvowel('This website is for losers LOL!'), 'Ths wbst s fr lsrs LL!');
